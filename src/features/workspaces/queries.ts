64311ad97afcc71c1a58df646ddc702c557a9d2e
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-10-17 16:19:20 +0900

    Refactoring server queries.

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,31 +1,17 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { Account, Client, Databases, Query } from "node-appwrite";
+import { Query } from "node-appwrite";
 
 import { DATABASE_ID, MEMBERS_ID, WORKSPACE_ID } from "@/config";
 
-import { AUTH_COOKIE } from "../auth/constants";
-
 import { getMember } from "../members/utils";
 
+import { createSessionClient } from "@/lib/appwrite";
 import { Workspace } from "./types";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) {
-      return { documents: [], total: 0 };
-    }
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { databases, account } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
@@ -56,26 +42,16 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) return null;
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { databases, account } = await createSessionClient();
     const user = await account.get();
 
     const member = await getMember({
       databases,
       userId: user.$id,
-      workspaceId
-    })
+      workspaceId,
+    });
 
-    if(!member) {
+    if (!member) {
       return null;
     }
 
