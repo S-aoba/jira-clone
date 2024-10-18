2144e6bcbc9472b158cc12b666a3d3218892a7ca
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-10-18 18:35:17 +0900

    Add delete functionality

--- src/features/workspaces/components/workspace-avatar.tsx
+++ src/features/workspaces/components/workspace-avatar.tsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 interface WorkspaceAvatarProps {
   image?: string;
   name: string;
-  classname?: string;
+  className?: string;
 }
 
 export const WorkspaceAvatar = ({
   image,
   name,
-  classname,
+  className,
 }: WorkspaceAvatarProps) => {
   if (image) {
     return (
       <div
-        className={cn("size-10 relative rounded-md overflow-hidden", classname)}
+        className={cn("size-10 relative rounded-md overflow-hidden", className)}
       >
         <Image src={image} alt={name} fill className="object-cover" />
       </div>
@@ -24,7 +24,7 @@ export const WorkspaceAvatar = ({
   }
 
   return (
-    <Avatar className={cn("size-10 rounded-md", classname)}>
+    <Avatar className={cn("size-10 rounded-md", className)}>
       <AvatarFallback className="text-white bg-blue-600 font-semibold text-lg uppercase rounded-md">
         {name[0]}
       </AvatarFallback>
